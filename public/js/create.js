b93c65fdfaa860f508290ba5674aeec22b46a804
Paulo Henrique Aguiar <phaguiardm@gmail.com>
2016-05-22 02:45:18 -0300

    Home e Create Page

---
+++ public/js/create.js
@@ -0,0 +1,89 @@
+$(document).ready(function() {
+    return $.getJSON('/newpresentation', function(data) {
+        $('body').data('id', data['id']);
+        $('body').data('title', data['title']);
+        $('body').data('size', data['size']);
+
+        return SetCurrentPage(0);
+    });
+});
+
+$('#changeTitle').on('click', function(e) {
+    $('#presentationTitleInput').val($('body').data('title'));
+});
+
+$('#saveTitle').on('click', function(e) {
+    var payload = { 'title': $('#presentationTitleInput').val() };
+
+    $.post('/title/' + $('body').data('id'), payload, function(response) {
+        $('body').data('title', response['title']);
+        return UpdateNavigationControls();
+    }, 'json');
+});
+
+$('#changeComment').on('click', function(e) {
+    $('#presentationCommentInput').val($('body').data('comment'));
+});
+
+$('#saveComment').on('click', function(e) {
+    var payload = { 'comment': $('#presentationCommentInput').val() };
+
+    $.post('/comment/' + $('body').data('id') + '/' + $('body').data('index'), payload, function(response) {
+        $('body').data('comment', response['comment']);
+        return UpdateNavigationControls();
+    }, 'json');
+});
+
+$('#createPage').on('click', function(e) {
+    $.getJSON('/page/' + $('body').data('id'), function(response) {
+        $('body').data('size', response['size']);
+        return UpdateNavigationControls();
+    }, 'json');
+});
+
+$('#next').on('click', function(e) {
+    return SetCurrentPage($('body').data('index') + 1);
+});
+
+$('#prev').on('click', function(e) {
+    return SetCurrentPage($('body').data('index') - 1);
+});
+
+function SetCurrentPage(pageNum, callback)
+{
+    return $.getJSON('/page/' + $('body').data('id') + '/' + pageNum, function(response) {
+        $('body').data('comment', response['comment']);
+        $('body').data('index', response['id']);
+        console.log(response);
+        $("#pageView").attr("src", response['img_path']);
+        return UpdateNavigationControls(callback);
+    }, 'json');
+}
+
+function UpdateNavigationControls(callback)
+{
+    var numOfPages = $('body').data('size');
+    $('#numOfPages').text(numOfPages);
+    var currentPage = $('body').data('index');
+    $('#currentPage').text(currentPage + 1);
+
+    if (currentPage > 0) {
+        $('#prev').removeClass('disabled');
+    } else if (!$('#prev').hasClass('disabled')){
+        $('#prev').addClass('disabled');
+    }
+
+    if (currentPage + 1 < numOfPages) {
+        $('#next').removeClass('disabled');
+    } else if (!$('#next').hasClass('disabled')){
+        $('#next').addClass('disabled');
+    }
+
+    $('#comment').text($('body').data('comment'));
+    $('#presentationId').html($('body').data('id'));
+    $('#presentationTitle').text($('body').data('title'));
+
+    if(callback)
+        return callback();
+    return;
+}
--- server.js
+++ server.js
@@ -1,11 +1,201 @@
-var express = require('express')
-var path = require('path')
+var express = require('express');
+var path = require('path');
+var morgan = require('morgan');
+var fileDB = require('json-file-db');
+var fs = require('fs');
+var fsE = require('fs-extra');
+var bodyParser = require('body-parser')
 
-var app = express()
+var app = express();
 
+if (!fs.existsSync('data')){
+    fs.mkdirSync('data');
+}
+if (!fs.existsSync('data/presentation')){
+    fs.mkdirSync('data/presentation');
+}
+
+var controlStorage = fileDB('./data/control.json');
+var presentationStorage = fileDB('./data/presentations.json');
+
+controlStorage.get(function(err, data) {
+    if (data.length == 0) {
+        controlStorage.put({
+            'id': 'control',
+            'presentation_id': 0,
+            'img_id': 1,
+        }, function(err) {
+            if (err)
+                return console.log("Error saving control file.\n" + err);
+            console.log("Control file created!");
+        });
+    }
+});
+
+// Express app setup
+app.set('view engine', 'ejs');
 app.set('port', process.env.PORT || 3000);
+
+// Start serving static files for test purposes
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
 
-app.use('/', express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-app.listen(app.get('port'), function() { console.log('listening on port ' + app.get('port'))});
+app.use(morgan('dev'))
+console.log(__dirname);
+
+// index page
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+app.get('/create', function(req, res) {
+    res.render('pages/create');
+});
+
+app.get('/newpresentation', function(req, res) {
+    return controlStorage.getSingle('control', function(err, data) {
+        if (err) {
+            console.log('Error getting json from control storage.');
+            return res.json({
+                id: 0
+            });
+        }
+        console.log(data);
+        var newId = data['presentation_id'];
+        data['presentation_id'] = newId + 1;
+        console.log('a = ' + newId);
+        return controlStorage.put(data, function(err) {
+            var payload = {
+                'id': newId,
+                'title': 'Nova Apresentação',
+                'size': 1,
+            };
+
+            return presentationStorage.put(payload, function(err) {
+                if (err)
+                    throw err;
+
+                var currentPresentationStorage = fileDB('data/presentation/p_' + newId + '_items.json');
+                var currentItem = {
+                    'id': 0,
+                    'comment': '',
+                    'img_path': 'user_img/0.png',
+                };
+
+                return currentPresentationStorage.put(currentItem, function(err) {
+                    if (err)
+                        throw err;
+
+                    console.log('Presentation #' + newId + ' created.');
+                    return res.json(payload);
+                });
+            });
+        });
+    });
+});
+
+app.get('/item/:id/:index', function(req, res) {
+    var id = req.params.id;
+    var index = req.params.index;
+
+    var currentPresentationStorage = fileDB('data/presentation/p_' + id + '_items.json');
+
+    return currentPresentationStorage.getSingle({'id': Number(index)}, function(err, data) {
+        if (err)
+            throw err;
+
+        console.log('Item #' + index + ' of Presentation #' + id + ' loaded.');
+        return res.json(data);
+    });
+});
+
+app.post('/comment/:id/:index', function(req, res) {
+    var id = req.params.id;
+    var index = req.params.index;
+    var payload = req.body;
+
+    var currentPresentationStorage = fileDB('data/presentation/p_' + id + '_items.json');
+    return currentPresentationStorage.getSingle({'id': Number(index)}, function(err, data) {
+        if (err)
+            throw err;
+        data['comment'] = payload['comment'];
+        return currentPresentationStorage.put(data, function(err) {
+            if(err)
+                throw err;
+            return res.json(data);
+        });
+    });
+});
+
+app.post('/title/:id', function(req, res) {
+    var id = req.params.id;
+    var payload = req.body;
+
+    return presentationStorage.getSingle({'id': Number(id)}, function(err, data) {
+        if (err)
+            throw err;
+
+        data['title'] = payload['title'];
+        return presentationStorage.put(data, function(err) {
+            if(err)
+                throw err;
+            return res.json(data);
+        });
+    });
+});
+
+app.get('/page/:id', function(req, res) {
+    var id = req.params.id;
+
+    var currentPresentationStorage = fileDB('data/presentation/p_' + id + '_items.json');
+
+    return presentationStorage.getSingle({'id': Number(id)}, function(err, data) {
+        if (err)
+            throw err;
+
+        data['size'] = data['size'] + 1;
+        return presentationStorage.put(data, function(err) {
+            if (err)
+                throw err;
+
+            var newItem = {
+                'id': data['size'] - 1,
+                'comment': '',
+                'img_path': 'user_img/blank.png',
+            };
+
+            return currentPresentationStorage.put(newItem, function(err) {
+                if (err)
+                    throw err;
+
+                console.log('Presentation #' + id + ' updated.');
+                return res.json(data);
+            });
+        });
+    });
+});
+
+app.get('/page/:id/:index', function(req, res) {
+    var id = req.params.id;
+    var index = req.params.index;
+
+    var currentPresentationStorage = fileDB('data/presentation/p_' + id + '_items.json');
+
+    return currentPresentationStorage.getSingle({'id': Number(index)}, function(err, data) {
+        if (err)
+            throw err;
+
+        console.log(data);
+        return res.json(data);
+    });
+});
+
+app.listen(app.get('port'),
+    function() {
+        console.log('listening on port ' + app.get('port'))
+    });
